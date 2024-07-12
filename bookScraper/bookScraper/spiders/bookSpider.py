5763a879d230dd35b89ec664ca720c564ec366e4
jack-njenga <jacknjenga99@gmail.com>
2024-07-12 12:40:45 +0300

    crawling and scraping each book page

--- bookScraper/bookScraper/spiders/bookSpider.py
+++ bookScraper/bookScraper/spiders/bookSpider.py
@@ -13,24 +13,25 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css("article.product_pod")
 
         for book in books:
-            book_url = f"https://books.toscrape.com/{book.css('h3 a::attr(href)').get()}"
+            book_url = book.css('h3 a::attr(href)').get()
 
-            yield {
-                "name": book.css("h3 a::text").get(),
-                "price": book.css("div.product_price p::text").get(),
-            }
+            if "catalogue/" in book_url:
+                book_url = "https://books.toscrape.com/" + book_url
+            else:
+                book_url = "https://books.toscrape.com/catalogue/" + book_url
+            yield response.follow(book_url, callback=self.parse_book)
+
         next_page = response.css("li.next a::attr(href)").get()
 
         if next_page:
             if "catalogue/" in next_page:
                 next_page_url = "https://books.toscrape.com/" + next_page
             else:
-                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
-            
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page            
             yield response.follow(next_page_url, callback=self.parse)
 
 
-    def parse_book(self, book_url, response):
+    def parse_book(self, response):
         """
         Scrapes each books page
         """
